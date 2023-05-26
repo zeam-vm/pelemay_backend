118e8f62c22cc04e1f54909f1a4d3214c1701a5c
Susumu Yamazaki <zacky1972@gmail.com>
2023-05-27 06:33:27 +0900

    write description of DecoratingBackend

--- backends/decorating_backend/lib/decorating_backend.ex
+++ backends/decorating_backend/lib/decorating_backend.ex
@@ -1,6 +1,11 @@
 defmodule DecoratingBackend do
   @moduledoc """
-  Documentation for `DecoratingBackend`.
+  A backend to decorate the specified `based_backend` with the functions
+  before and after a set of functions in the backend. The set can be specified
+  with the style of
+  [AspectJ, which is an AOP language](https://en.wikipedia.org/wiki/Aspect-oriented_programming),
+  and with grouping written in [hexdocs of Nx](https://hexdocs.pm/nx/Nx.html),
+  for example, Aggregates, Backend, Conversion, and so on.
   """
 
   @doc """
