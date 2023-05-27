799a2a5926f5f92e1616db9743a81f5843acf4a7
Susumu Yamazaki <zacky1972@gmail.com>
2023-05-27 11:31:11 +0900

    make LoggingBackend depend BackendDecorator

--- backends/logging_backend/mix.exs
+++ backends/logging_backend/mix.exs
@@ -21,8 +21,7 @@ defmodule LoggingBackend.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:backend_decorator, path: "../../utilities/backend_decorator"}
     ]
   end
 end
