db5194c719ebaa6b23a83fb4c50e7df204e54913
Susumu Yamazaki <zacky1972@gmail.com>
2023-06-13 05:12:16 +0900

    test: remove hello
    related to #65

--- utilities/node_activator/lib/node_activator.ex
+++ utilities/node_activator/lib/node_activator.ex
@@ -2,17 +2,4 @@ defmodule NodeActivator do
   @moduledoc """
   Documentation for `NodeActivator`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> NodeActivator.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- utilities/node_activator/test/node_activator_test.exs
+++ utilities/node_activator/test/node_activator_test.exs
@@ -1,8 +1,4 @@
 defmodule NodeActivatorTest do
   use ExUnit.Case
   doctest NodeActivator
-
-  test "greets the world" do
-    assert NodeActivator.hello() == :world
-  end
 end
