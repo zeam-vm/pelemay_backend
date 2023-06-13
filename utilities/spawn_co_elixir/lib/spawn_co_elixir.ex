b04d27fc94688edd091384393444211bf9f5d173
Susumu Yamazaki <zacky1972@gmail.com>
2023-06-14 07:44:52 +0900

    feat: implement CoElixir
    related to #75

--- utilities/spawn_co_elixir/lib/spawn_co_elixir.ex
+++ utilities/spawn_co_elixir/lib/spawn_co_elixir.ex
@@ -2,4 +2,11 @@ defmodule SpawnCoElixir do
   @moduledoc """
   Documentation for `SpawnCoElixir`.
   """
+
+  def init() do
+    :ets.new(:spawn_co_elixir_co_elixir_lookup, [:set, :public, :named_table])
+  end
+
+  def run(options \\ [name: "co_elixir"]) do
+  end
 end
---
+++ utilities/spawn_co_elixir/lib/spawn_co_elixir/co_elixir.ex
@@ -0,0 +1,103 @@
+defmodule SpawnCoElixir.CoElixir do
+  @moduledoc """
+  SpawnCoElixir.CoElixir
+  """
+
+  use GenServer
+
+  @impl true
+  def init(a_process \\ %{options: [name: "co_elixir"]}) do
+    {:ok, a_process}
+  end
+
+  @impl true
+  def handle_cast(:spawn_co_elixir, a_process) do
+    unless a_process[:running] do
+      spawn_link(fn ->
+        spawn_co_elixir(a_process)
+        GenServer.cast(:exit, a_process)
+      end)
+    end
+
+    {
+      :noreply,
+      Map.put(a_process, :running, true)
+    }
+  end
+
+  @impl true
+  def handle_cast(:exit, a_process) do
+    {
+      :noreply,
+      Map.put(a_process, :running, false)
+    }
+  end
+
+  def start_link(a_process \\ %{options: [name: "co_elixir"]}) do
+    {:ok, pid} = GenServer.start_link(__MODULE__, a_process)
+    GenServer.cast(pid, :spawn_co_elixir)
+  end
+
+  def workers() do
+    :ets.tab2list(:spawn_co_elixir_co_elixir_lookup)
+    |> Enum.map(fn {node, _pid} -> node end)
+  end
+
+  defp spawn_co_elixir(a_process) do
+    options = a_process[:options]
+
+    worker_node =
+      options[:name]
+      |> NodeActivator.name_with_random_key()
+      |> concat_node_name_with_hostname()
+
+    :ets.insert(:spawn_co_elixir_co_elixir_lookup, {worker_node, self()})
+
+    deps =
+      case options[:deps] do
+        nil -> []
+        deps -> deps
+      end
+
+    {_, 0} =
+      System.cmd(
+        "elixir",
+        [
+          "--name",
+          worker_node,
+          "-e",
+          """
+          defmodule SpawnCoElixir.CoElixir.Worker do
+            def run() do
+              Mix.install(#{inspect(deps)})
+
+              receive do
+                :end -> :ok
+              end
+            end
+          end
+
+          case Node.connect(:"#{node()}") do
+            true ->
+              SpawnCoElixir.CoElixir.Worker.run()
+              :ok
+
+            _ -> raise RuntimeError, "Node cannot connect."
+          end
+          """
+        ],
+        into: IO.stream()
+      )
+  end
+
+  defp hostname() do
+    node()
+    |> Atom.to_string()
+    |> String.split("@")
+    |> Enum.at(1)
+  end
+
+  defp concat_node_name_with_hostname(name) do
+    "#{name}@#{hostname()}"
+  end
+end
