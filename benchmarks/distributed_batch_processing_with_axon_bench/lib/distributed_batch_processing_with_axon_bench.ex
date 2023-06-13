18bd3767a6f107b52d57d14687e8c663630ae1df
Susumu Yamazaki <zacky1972@gmail.com>
2023-06-14 04:03:05 +0900

    feat: create run to activate node with name "primary"
    related to #67

--- benchmarks/distributed_batch_processing_with_axon_bench/lib/distributed_batch_processing_with_axon_bench.ex
+++ benchmarks/distributed_batch_processing_with_axon_bench/lib/distributed_batch_processing_with_axon_bench.ex
@@ -2,4 +2,8 @@ defmodule DistributedBatchProcessingWithAxonBench do
   @moduledoc """
   Documentation for `DistributedBatchProcessingWithAxonBench`.
   """
+
+  def run() do
+    NodeActivator.run("primary")
+  end
 end
