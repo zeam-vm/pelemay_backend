e580592a90bbb4732b1dbc7de6888e3cc7183cbb
Susumu Yamazaki <zacky1972@gmail.com>
2023-06-14 04:07:08 +0900

    feat: create a subproject utilities/spawn_co_elixir
    related to #75

---
+++ utilities/spawn_co_elixir/lib/spawn_co_elixir.ex
@@ -0,0 +1,18 @@
+defmodule SpawnCoElixir do
+  @moduledoc """
+  Documentation for `SpawnCoElixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SpawnCoElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
