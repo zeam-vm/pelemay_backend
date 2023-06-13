71265d03a8d8de87da1998386e606214ee5d593d
Susumu Yamazaki <zacky1972@gmail.com>
2023-06-14 04:11:00 +0900

    remove hello
    related to #75

--- utilities/spawn_co_elixir/lib/spawn_co_elixir.ex
+++ utilities/spawn_co_elixir/lib/spawn_co_elixir.ex
@@ -2,17 +2,4 @@ defmodule SpawnCoElixir do
   @moduledoc """
   Documentation for `SpawnCoElixir`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> SpawnCoElixir.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
