f0519070d36b0fa10a7c4c752d1d4e2ad6bb0da9
Susumu Yamazaki <zacky1972@gmail.com>
2023-06-18 23:20:47 +0900

    Fail to activate Node on Windows #104 (#114)
    * build: add macOS and Windows
    
    related to #102
    
    * feat: Logger.error("epmd not found.")
    
    related to #104
    
    * fix: try to fix issue to fail to activate Node on Windows
    
    related to #104
    
    * apply mix format
    
    * extract launch_epmd to fix credo issue
    
    related to #104
    
    * change error messages
    
    related to #104
    
    * fix: try to fix issue on test run (NodeActivatorTest)
    
    related to #104
    
    * Fail to Node.start/1 due to execute it before launch_epmd/2 #111 (#113)
    
    * fix: issue to fail to Node.start/1 due to execute it before launch_epmd/2
    * fix: Fail to Node.start/1 due to execute it before launch_epmd/2 in case of windows
    * fix: Fail to Node.start/1 due to execute it before launch_epmd/2
    
    related to #111

--- utilities/node_activator/lib/node_activator.ex
+++ utilities/node_activator/lib/node_activator.ex
@@ -4,6 +4,8 @@ defmodule NodeActivator do
   """
   require Logger
 
+  @epmd_port 4369
+
   @doc """
   Activates Node.
   """
@@ -12,10 +14,41 @@ defmodule NodeActivator do
     if Node.alive?() do
       {:ok, Node.self()}
     else
-      launch_epmd()
+      case :os.type() do
+        {:unix, _} -> launch_epmd(port: @epmd_port)
+        {:win32, _} -> launch_epmd(port: @epmd_port, daemon: false)
+      end
+
+      Logger.info("wait launching epmd...")
+      wait_launching_epmd(5)
+      Logger.info("done.")
       name = name |> name_with_random_key() |> String.to_atom()
       Logger.info("Node.start(#{name})")
       Node.start(name)
+    end
+  end
+
+  defp wait_launching_epmd(0), do: raise(RuntimeError, "Fail to launch epmd.")
+
+  defp wait_launching_epmd(count) do
+    unless can_connect?(@epmd_port) do
+      Process.sleep(1000)
+      wait_launching_epmd(count - 1)
+    end
+
+    :ok
+  end
+
+  defp can_connect?(port) do
+    case :gen_tcp.connect(~c'localhost', port, [:binary, active: false], 1000) do
+      {:ok, socket} ->
+        Logger.info("active.")
+        :gen_tcp.close(socket)
+        true
+
+      {:error, reason} ->
+        Logger.debug("Fail to connect due to #{inspect(reason)}.")
+        false
     end
   end
 
@@ -29,19 +62,23 @@ defmodule NodeActivator do
     hostname = System.find_executable("hostname")
 
     if is_nil(hostname) do
-      raise RuntimeError, "Fail to execute the \"execute\" command."
+      raise RuntimeError, "Fail to find the \"hostname\" command."
     else
-      {result, exit_code} = System.cmd(hostname, ["-f"])
+      {result, exit_code} =
+        case :os.type() do
+          {:unix, _} -> System.cmd(hostname, ["-f"])
+          {:win32, _} -> System.cmd(hostname, [])
+        end
 
       if exit_code == 0 do
         String.trim(result)
       else
-        raise RuntimeError, "Fail to execute the \"execute\" command."
+        raise RuntimeError, "Fail to execute the \"hostname\" command."
       end
     end
   end
 
-  @spec launch_epmd(keyword) :: :ok | :error
+  @spec launch_epmd(keyword) :: :ok
   def launch_epmd(options \\ [daemon: true]) do
     options =
       options
@@ -68,18 +105,23 @@ defmodule NodeActivator do
     epmd_path = System.find_executable("epmd")
 
     if is_nil(epmd_path) do
-      :error
+      Logger.error("Fail to find epmd.")
+      raise RuntimeError, "Fail to find epmd."
     else
-      {result, exit_code} = System.cmd(epmd_path, options)
+      spawn_link(fn -> launch_epmd(epmd_path, options) end)
+      :ok
+    end
+  end
 
-      if exit_code == 0 do
-        Logger.info("epmd #{Enum.join(options, " ")}: #{result}")
-        :ok
-      else
-        # credo:disable-for-next-line
-        Logger.error("epmd #{Enum.join(options, " ")}: #{result}", error_code: exit_code)
-        :error
-      end
+  def launch_epmd(epmd_path, options) do
+    {result, exit_code} = System.cmd(epmd_path, options, parallelism: true)
+
+    if exit_code == 0 do
+      Logger.info("epmd #{Enum.join(options, " ")}: #{result}")
+    else
+      # credo:disable-for-next-line
+      Logger.error("epmd #{Enum.join(options, " ")}: #{result}", error_code: exit_code)
+      raise RuntimeError, "Fail to launch epmd #{Enum.join(options, " ")}: #{result}"
     end
   end
 end
