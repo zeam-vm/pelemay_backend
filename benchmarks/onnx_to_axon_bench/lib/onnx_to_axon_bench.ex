3cb31b2dccd9f7aaf84a1732084580bcadfbd58b
Masatoshi Nishiguchi <mnishiguchi@users.noreply.github.com>
2023-06-18 15:41:24 -0400

    refactor(OnnxToAxonBench): factor out http utils (#115)
    * refactor(OnnxToAxonBench): factor out http utils
    * refactor: remove import
    * refactor: move http.ex into onnx_to_axon_bench/util
    
    related to #115
    
    ---------
    
    Co-authored-by: Susumu Yamazaki <zacky1972@gmail.com>

--- benchmarks/onnx_to_axon_bench/lib/onnx_to_axon_bench.ex
+++ benchmarks/onnx_to_axon_bench/lib/onnx_to_axon_bench.ex
@@ -16,7 +16,7 @@ defmodule OnnxToAxonBench do
 
     inputs =
       onnx_uri
-      |> Enum.map(fn url -> basename_from_uri(url) end)
+      |> Enum.map(fn url -> OnnxToAxonBench.Utils.HTTP.basename_from_uri(url) end)
       |> Enum.map(fn basename -> {basename, basename} end)
       |> Map.new()
 
@@ -56,70 +56,6 @@ defmodule OnnxToAxonBench do
     Path.join(priv(), "data")
   end
 
-  @spec download(Req.url(), keyword()) :: {:ok, binary() | term()} | {:error, Exception.t()}
-  def download(source_url, req_options \\ []) do
-    case Req.get(source_url, [finch_request: &finch_request/4] ++ req_options) do
-      {:ok, response} -> {:ok, response.body}
-      {:error, reason} -> {:error, reason}
-    end
-  end
-
-  @spec download!(Req.url(), keyword()) :: binary() | term()
-  def download!(source_url, req_options \\ []) do
-    Req.get!(source_url, [finch_request: &finch_request/4] ++ req_options).body
-  end
-
-  defp finch_request(req_request, finch_request, finch_name, finch_options) do
-    acc = Req.Response.new()
-
-    case Finch.stream(finch_request, finch_name, acc, &handle_message/2, finch_options) do
-      {:ok, response} -> {req_request, response}
-      {:error, reason} -> {req_request, reason}
-    end
-  end
-
-  defp handle_message({:status, status}, response), do: %{response | status: status}
-
-  defp handle_message({:headers, headers}, response) do
-    {_, total_size} =
-      Enum.find(headers, fn
-        {"content-length", v} -> String.to_integer(v)
-        {_, _} -> nil
-      end)
-
-    response
-    |> Map.put(:headers, headers)
-    |> Map.put(:private, %{total_size: String.to_integer(total_size), downloaded_size: 0})
-  end
-
-  defp handle_message({:data, data}, response) do
-    total_size = response.private.total_size
-
-    if total_size > 0 do
-      handle_message_data(data, response)
-    else
-      response
-    end
-  end
-
-  defp handle_message_data(data, response) do
-    new_downloaded_size = response.private.downloaded_size + byte_size(data)
-    ProgressBar.render(new_downloaded_size, response.private.total_size, suffix: :bytes)
-
-    response
-    |> Map.update!(:body, &(&1 <> data))
-    |> Map.update!(:private, &%{&1 | downloaded_size: new_downloaded_size})
-  end
-
-  @spec basename_from_uri(Req.url()) :: String.t()
-  def basename_from_uri(url) when is_binary(url) do
-    Path.basename(url)
-  end
-
-  def basename_from_uri(url) do
-    URI.parse(url) |> Map.get(:path) |> Path.basename()
-  end
-
   @spec init() :: :ok
   def init() do
     File.mkdir_p!(path_models_onnx())
@@ -127,35 +63,15 @@ defmodule OnnxToAxonBench do
     File.mkdir_p!(path_data())
   end
 
-  @spec download_files(list(Req.url()), String.t()) :: list(String.t())
-  def download_files(files, dst_path) do
-    Enum.map(files, fn url ->
-      basename = basename_from_uri(url)
-
-      dst_path = Path.join(dst_path, basename)
-
-      dst_path
-      |> File.exists?()
-      |> case do
-        true ->
-          Logger.info("File #{basename} has already been downloaded.")
-
-        false ->
-          Logger.info("File #{basename} will be downloaded...")
-          download!(url, output: dst_path, max_redirects: 5, redirect_log_level: false)
-      end
-    end)
-  end
-
   @spec setup_onnx(list(String.t())) :: list(String.t())
   def setup_onnx(files) do
-    download_files(files, path_models_onnx())
+    OnnxToAxonBench.Utils.HTTP.download_files(files, path_models_onnx())
   end
 
   @spec setup_data(list(String.t())) ::
           list(:ok | {:ok, list({charlist(), String.t()})} | {:error, any()})
   def setup_data(files) do
-    download_files(files, path_data())
+    OnnxToAxonBench.Utils.HTTP.download_files(files, path_data())
 
     files
     |> Flow.from_enumerable(max_demand: 1)
@@ -166,7 +82,7 @@ defmodule OnnxToAxonBench do
   @spec extract_from_url(Req.url()) ::
           :ok | {:ok, list({charlist(), String.t()})} | {:error, any()}
   def extract_from_url(url) do
-    Path.join(path_data(), basename_from_uri(url))
+    Path.join(path_data(), OnnxToAxonBench.Utils.HTTP.basename_from_uri(url))
     |> File.read!()
     |> extract_tar_from_string()
   end
