56bc2c731ea0109857d93a52df2684f9c3c1875c
山崎進 <zacky@zackym2pro01.local>
2023-07-06 21:19:27 +0900

    build: set up docs, packages, name and description

--- utilities/node_activator/mix.exs
+++ utilities/node_activator/mix.exs
@@ -1,13 +1,19 @@
 defmodule NodeActivator.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @source_url "https://github.com/zeam-vm/pelemay_backend"
+
   def project do
     [
       app: :node_activator,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.14",
-      start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      name: "NodeActivator",
+      description: "A module to activate VM nodes",
+      package: package()
     ]
   end
 
@@ -24,6 +30,24 @@ defmodule NodeActivator.MixProject do
       {:dialyxir, "~> 1.3", only: [:dev], runtime: false},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.29", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Susumu Yamazaki", "Masatoshi Nishiguchi"],
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => @source_url}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "NodeActivator",
+      source_url_patterm: "#{@source_url}/blob/v#{@version}/utilities/node_activator/%{path}#L%{line}",
+      extras: [
+        "README.md"
+      ]
     ]
   end
 end
