610350a3640a469059abdb6b83ba09555a986735
Masatoshi Nishiguchi <mnishiguchi@users.noreply.github.com>
2023-07-22 17:01:21 -0400

    refactor(SpawnCoElixir): add CoElixirWorkerSpawner and refactor (#180)
    * handle_call :exit
    
    * handle_call :worker_node
    
    * simplify handle_call :exit
    
    * add CoElixirWorkerSpawner and refactor
    
    * switch name and sname

--- utilities/spawn_co_elixir/lib/spawn_co_elixir/co_elixir.ex
+++ utilities/spawn_co_elixir/lib/spawn_co_elixir/co_elixir.ex
@@ -4,10 +4,11 @@ defmodule SpawnCoElixir.CoElixir do
   use GenServer
   require Logger
   alias SpawnCoElixir.CoElixirLookup
+  alias SpawnCoElixir.CoElixirWorkerSpawner
 
   ## Client API
 
-  @spec start_link([SpawnCoElixir.co_elixir_option()]) :: {:ok, pid}
+  @spec start_link([SpawnCoElixir.co_elixir_option()]) :: GenServer.on_start()
   def start_link(options \\ []) do
     server_options = [
       co_elixir_name: Keyword.fetch!(options, :co_elixir_name),
@@ -17,21 +18,19 @@ defmodule SpawnCoElixir.CoElixir do
     ]
 
     NodeActivator.run(server_options[:host_name])
-    {:ok, pid} = GenServer.start_link(__MODULE__, server_options)
-    GenServer.cast(pid, :spawn_co_elixir)
-    {:ok, pid}
+    GenServer.start_link(__MODULE__, server_options)
   end
 
-  @spec stop(node) :: :ok
+  @spec stop(node) :: :ok | {:error, any}
   def stop(worker_node) do
     case CoElixirLookup.get_worker_pid(worker_node) do
       nil ->
         Logger.warning("Not found worker_node #{worker_node}.")
-        :ok
+        {:error, :not_found}
 
       pid when is_pid(pid) ->
         Logger.info("Found worker_node {#{worker_node}, #{inspect(pid)}}")
-        GenServer.cast(pid, :exit)
+        GenServer.call(pid, :exit_co_elixir)
     end
   end
 
@@ -41,109 +40,82 @@ defmodule SpawnCoElixir.CoElixir do
   def init(options \\ []) do
     a_process = %{options: options, running: false, worker_node: nil}
 
-    {:ok, a_process}
+    {:ok, a_process, {:continue, :spawn_co_elixir}}
   end
 
   @impl true
-  def handle_cast(:spawn_co_elixir, a_process) do
-    unless a_process.running do
-      ret = self()
-      spawn_link(fn -> handle_cast_s(spawn_co_elixir(ret, a_process.options), ret) end)
-    end
-
-    {:noreply, %{a_process | running: true}}
-  end
-
-  @impl true
-  def handle_cast({:worker_node, worker_node}, a_process) do
-    Logger.info("Register worker #{inspect(worker_node)}")
-
-    {:noreply, %{a_process | worker_node: worker_node}}
-  end
-
-  @impl true
-  def handle_cast(:exit, a_process) do
-    case Map.get(a_process, :worker_node) do
-      nil ->
-        Logger.error("Not found worker_node")
-
-        {:noreply, %{a_process | running: false}}
-
-      :stopped ->
-        {:noreply, %{a_process | running: false, worker_node: nil}}
-
-      worker_node ->
-        Logger.info("Exit #{inspect(worker_node)}")
-        Node.spawn(worker_node, System, :halt, [])
-        CoElixirLookup.delete_entry(worker_node)
-
-        {:noreply, %{a_process | running: false, worker_node: :stopped}}
-    end
-  end
-
-  defp handle_cast_s({:ok, 0}, ret) do
-    Logger.info("Exit CoElixir.")
-    GenServer.cast(ret, :exit)
-  end
-
-  defp handle_cast_s({:ok, _exit_code}, ret) do
-    Logger.info("Reboot CoElixir.")
-    GenServer.cast(ret, :exit)
-    GenServer.cast(ret, :spawn_co_elixir)
-  end
-
-  defp spawn_co_elixir(pid, options) do
-    code = Keyword.fetch!(options, :code)
-    deps = Keyword.fetch!(options, :deps)
+  def handle_continue(:spawn_co_elixir, a_process) do
+    options = a_process.options
     node_name_prefix = Keyword.fetch!(options, :co_elixir_name)
-
     worker_node = NodeActivator.Utils.generate_node_name(node_name_prefix)
-    :ok = CoElixirLookup.put_entry(worker_node, pid)
+    this_pid = self()
+    this_node = Node.self()
 
-    try do
-      GenServer.cast(pid, {:worker_node, worker_node})
+    if a_process.running do
+      {:noreply, a_process}
+    else
+      Logger.info("spawning #{inspect(worker_node)}")
 
-      program = """
-      defmodule SpawnCoElixir.CoElixir.Worker do
-        def run() do
-          Mix.install(#{inspect(deps)})
+      spawn_link(fn ->
+        try do
+          :ok = GenServer.call(this_pid, {:register_worker_node, worker_node})
 
-          #{code}
+          case CoElixirWorkerSpawner.run(this_node, worker_node, options) do
+            :ok ->
+              Logger.info("spawned #{inspect(worker_node)}")
+              :ok = GenServer.call(this_pid, :exit_co_elixir)
 
-          receive do
-            :end -> :ok
+            {:error, exit_code} ->
+              Logger.info("could not spawn #{inspect(worker_node)}: exit code #{exit_code}")
+              :ok = GenServer.call(this_pid, :reboot_co_elixir)
           end
+        after
+          :ok = GenServer.call(this_pid, {:deregister_worker_node, worker_node})
         end
-      end
+      end)
 
-      case Node.connect(:"#{node()}") do
-        true ->
-          SpawnCoElixir.CoElixir.Worker.run()
-          :ok
+      {:noreply, %{a_process | running: true}}
+    end
+  end
 
-        _ -> raise RuntimeError, "Node #{node()} cannot connect."
-      end
-      """
+  @impl true
+  def handle_call({:register_worker_node, worker_node}, {pid_from, _}, a_process) do
+    Logger.info("registering #{inspect(worker_node)}")
+    :ok = CoElixirLookup.put_entry(worker_node, pid_from)
 
-      Logger.info("spawn #{inspect(worker_node)}...")
+    {:reply, :ok, %{a_process | worker_node: worker_node}}
+  end
 
-      {_result, exit_code} =
-        System.cmd(
-          "elixir",
-          [
-            "--name",
-            Atom.to_string(worker_node),
-            "-e",
-            program
-          ],
-          into: IO.stream()
-        )
+  @impl true
+  def handle_call({:deregister_worker_node, worker_node}, {_pid_from, _}, a_process) do
+    Logger.info("deregistering #{inspect(worker_node)}")
+    :ok = CoElixirLookup.delete_entry(worker_node)
 
-      Logger.info("exit #{inspect(worker_node)} with exit_code #{exit_code}")
+    {:reply, :ok, %{a_process | worker_node: nil}}
+  end
 
-      {:ok, exit_code}
-    after
+  @impl true
+  def handle_call(:exit_co_elixir, {_pid_from, _}, a_process) do
+    Logger.info("exiting #{inspect(a_process.worker_node)}")
+    :ok = do_exit_co_elixir(a_process.worker_node)
+
+    {:reply, :ok, %{a_process | running: false, worker_node: nil}}
+  end
+
+  @impl true
+  def handle_call(:reboot_co_elixir, {_pid_from, _}, a_process) do
+    Logger.info("rebooting #{inspect(a_process.worker_node)}")
+    :ok = do_exit_co_elixir(a_process.worker_node)
+
+    {:noreply, %{a_process | running: false, worker_node: nil}, {:continue, :spawn_co_elixir}}
+  end
+
+  defp do_exit_co_elixir(worker_node) do
+    if worker_node do
+      _pid = Node.spawn(worker_node, System, :halt, [])
       :ok = CoElixirLookup.delete_entry(worker_node)
     end
+
+    :ok
   end
 end
---
+++ utilities/spawn_co_elixir/lib/spawn_co_elixir/co_elixir_worker_spawner.ex
@@ -0,0 +1,58 @@
+defmodule SpawnCoElixir.CoElixirWorkerSpawner do
+  @moduledoc false
+
+  @spec run(node, node, keyword) :: :ok | {:error, non_neg_integer}
+  def run(node_from, worker_node, options) do
+    code = Keyword.fetch!(options, :code)
+    deps = Keyword.fetch!(options, :deps)
+
+    name_or_sname =
+      if "#{worker_node}" =~ "." do
+        "--name"
+      else
+        "--sname"
+      end
+
+    {_, exit_status} =
+      System.cmd(
+        "elixir",
+        [
+          name_or_sname,
+          Atom.to_string(worker_node),
+          "-e",
+          build_program(node_from, code, deps)
+        ],
+        into: IO.stream()
+      )
+
+    if exit_status == 0 do
+      :ok
+    else
+      {:error, exit_status}
+    end
+  end
+
+  defp build_program(node_from, code, deps) do
+    """
+    defmodule SpawnCoElixir.CoElixir.Worker do
+      def run() do
+        Mix.install(#{inspect(deps)})
+
+        #{code}
+
+        receive do
+          :end -> :ok
+        end
+      end
+    end
+
+    case Node.connect(:"#{node_from}") do
+      true ->
+        SpawnCoElixir.CoElixir.Worker.run()
+        :ok
+
+      _ -> raise RuntimeError, "could not connect to #{node_from}"
+    end
+    """
+  end
+end
