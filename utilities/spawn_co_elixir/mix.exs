307f72fd679d089be227713b567a0330e04c3469
Susumu Yamazaki <zacky1972@gmail.com>
2023-07-23 04:01:10 +0900

    Release spawn co elixir (#176)
    * install ex_doc
    
    * set up docs
    
    * set up package

--- utilities/spawn_co_elixir/mix.exs
+++ utilities/spawn_co_elixir/mix.exs
@@ -1,13 +1,20 @@
 defmodule SpawnCoElixir.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @source_url "https://github.com/zeam-vm/pelemay_backend"
+
   def project do
     [
       app: :spawn_co_elixir,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      name: "SpawnCoElixir",
+      description: "SpawnCoElixir spawns cooperative Elixir nodes that are supervised.",
+      package: package()
     ]
   end
 
@@ -24,7 +31,27 @@ defmodule SpawnCoElixir.MixProject do
     [
       {:dialyxir, "~> 1.3", only: [:dev], runtime: false},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.29", only: :dev, runtime: false},
       {:node_activator, "~> 0.1"}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Susumu Yamazaki", "Masatoshi Nishiguchi"],
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => @source_url}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "SpawnCoElixir",
+      source_url_pattern:
+        "#{@source_url}/blob/v#{@version}/utilities/spawn_co_elixir/%{path}#L%{line}",
+      extras: [
+        "README.md"
+      ]
     ]
   end
 end
