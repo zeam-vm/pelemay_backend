8daa769174fee5a767e772ed48396a0764966346
Susumu Yamazaki <zacky1972@gmail.com>
2023-07-23 12:39:35 +0900

    fix: get fully qualified hostname when Win32

--- utilities/node_activator/lib/node_activator/utils.ex
+++ utilities/node_activator/lib/node_activator/utils.ex
@@ -27,6 +27,30 @@ defmodule NodeActivator.Utils do
       raise RuntimeError, "Fail to execute the \"hostname\" command."
     end
 
+    result
+    |> String.trim()
+    |> to_fully_qualified_hostname(:os.type())
+  end
+
+  defp to_fully_qualified_hostname(hostname, {:unix, _}), do: hostname
+
+  defp to_fully_qualified_hostname(hostname, {:win32, _}) do
+    ping_cmd = System.find_executable("ping")
+
+    if is_nil(ping_cmd) do
+      raise RuntimeError, "Fail to find the \"ping\" command."
+    end
+
+    {result, exit_code} = System.cmd(ping_cmd, ["/a", "/n", "1", hostname])
+
+    if exit_code > 0 do
+      raise RuntimeError, "Fail to execute the \"ping\" command."
+    end
+
     String.trim(result)
+    |> String.split("\n")
+    |> Enum.at(1)
+    |> then(&Regex.named_captures(~r/Reply from (?<ip>[0-9a-f:.]+)/, &1))
+    |> Map.get("ip")
   end
 end
