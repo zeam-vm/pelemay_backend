60a2de9ce87b59fca1a9e69d9cf3d29e8ff19ba7
Susumu Yamazaki <zacky1972@gmail.com>
2023-08-02 02:31:14 +0900

    release NodeActivator 0.2.0 (#191)
    * release NodeActivator 0.2.0
    
    * docs: create CHANGELOG.md

--- utilities/node_activator/mix.exs
+++ utilities/node_activator/mix.exs
@@ -1,7 +1,7 @@
 defmodule NodeActivator.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.2.0"
   @source_url "https://github.com/zeam-vm/pelemay_backend"
 
   def project do
@@ -47,7 +47,8 @@ defmodule NodeActivator.MixProject do
       source_url_pattern:
         "#{@source_url}/blob/v#{@version}/utilities/node_activator/%{path}#L%{line}",
       extras: [
-        "README.md"
+        "README.md",
+        "CHANGELOG.md"
       ]
     ]
   end
