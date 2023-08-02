731c685b1213c8ca1a0fc8a08cc8de4110f40188
Susumu Yamazaki <zacky1972@gmail.com>
2023-08-03 04:09:43 +0900

    build: install and set up dialyxir, credo and ex_doc
    related to #156

--- utilities/http_downloader/mix.exs
+++ utilities/http_downloader/mix.exs
@@ -1,13 +1,22 @@
 defmodule HttpDownloader.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :http_downloader,
-      version: "0.1.0",
-      elixir: "~> 1.15",
+      version: @version,
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      preferred_cli_env: %{
+        credo: :test,
+        dialyzer: :test,
+        docs: :docs,
+        "hex.publish": :docs,
+        "hex.build": :docs
+      }
     ]
   end
 
@@ -21,8 +30,9 @@ defmodule HttpDownloader.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:dialyxir, "~> 1.3", only: :test, runtime: false},
+      {:credo, "~> 1.7", only: :test, runtime: false},
+      {:ex_doc, "~> 0.29", only: :docs, runtime: false}
     ]
   end
 end
