69d3a82cb2b7e761b8c8ffe36cc018acad93a00a
Susumu Yamazaki <zacky1972@gmail.com>
2023-08-03 05:26:45 +0900

    remove hello
    related to #199

--- benchmarks/distributed_computing_bench/lib/distributed_computing_bench.ex
+++ benchmarks/distributed_computing_bench/lib/distributed_computing_bench.ex
@@ -2,17 +2,4 @@ defmodule DistributedComputingBench do
   @moduledoc """
   Documentation for `DistributedComputingBench`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> DistributedComputingBench.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- benchmarks/distributed_computing_bench/test/distributed_computing_bench_test.exs
+++ benchmarks/distributed_computing_bench/test/distributed_computing_bench_test.exs
@@ -1,8 +1,4 @@
 defmodule DistributedComputingBenchTest do
   use ExUnit.Case
   doctest DistributedComputingBench
-
-  test "greets the world" do
-    assert DistributedComputingBench.hello() == :world
-  end
 end
