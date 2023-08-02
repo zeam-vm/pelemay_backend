e6ddfeb56679487ec2d47b7f038d81bf06b9361e
Susumu Yamazaki <zacky1972@gmail.com>
2023-08-03 04:04:06 +0900

    build: create utilities/http_downloader
    related to #156

---
+++ utilities/http_downloader/lib/http_downloader.ex
@@ -0,0 +1,18 @@
+defmodule HttpDownloader do
+  @moduledoc """
+  Documentation for `HttpDownloader`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HttpDownloader.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ utilities/http_downloader/test/http_downloader_test.exs
@@ -0,0 +1,8 @@
+defmodule HttpDownloaderTest do
+  use ExUnit.Case
+  doctest HttpDownloader
+
+  test "greets the world" do
+    assert HttpDownloader.hello() == :world
+  end
+end
