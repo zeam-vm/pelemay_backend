bf43ae5b93ee014f8e5bf14c03df5302b286b76a
im <poson_hemp@yahoo.co.jp>
2024-10-21 06:52:06 +0900

    preparing compile

--- backends/pelemay_backend/mix.exs
+++ backends/pelemay_backend/mix.exs
@@ -13,7 +13,11 @@ defmodule PelemayBackend.MixProject do
       version: @version,
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      compilers: [:pelemay_backend, :elixir_make] ++ Mix.compilers(),
+      aliases: [
+        "compile.pelemay_backend": &compile/1
+      ]
     ]
   end
 
@@ -29,9 +33,37 @@ defmodule PelemayBackend.MixProject do
     [
       {:nx, ">= 0.5.0 and <= 0.6.2"},
       {:openblas_builder, "~> 0.1.0-dev", github: "zeam-vm/openblas_builder", branch: "main"},
+      {:elixir_make, "~> 0.6", runtime: false},
       {:nimble_parsec, "~> 1.2"},
       {:dialyxir, "~> 1.3", only: [:dev], runtime: false},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
+  end
+
+  defp compile(_) do
+    OpenBLASBuilder.extract_archive!()
+
+    OpenBLASBuilder.compile_matched!([
+      {"interface", "cblas_sscal"},
+      {"interface", "sscal"},
+      {"interface", "cblas_scopy"},
+      {"interface", "scopy"},
+      {"interface", "cblas_dscal"},
+      {"interface", "dscal"},
+      {"interface", "cblas_dcopy"},
+      {"interface", "dcopy"},
+      {"driver/others", "memory"},
+      {"driver/others", "blas_l1_thread"},
+      {"driver/others", "blas_server"},
+      {"driver/others", "parameter"},
+      {"driver/others", "openblas_env"},
+      {"driver/others", "openblas_error_handle"},
+      {"driver/others", "divtable"}
+    ])
+    |> Map.values()
+    |> Enum.join(" ")
+    |> then(&System.put_env("OPENBLAS_OBJ", &1))
+
+    {:ok, []}
   end
 end
